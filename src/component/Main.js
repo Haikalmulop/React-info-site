f091fe77a8ca239f80003e2c7ba0bb4e31da1f6e
carlsins <96352590+Haikalmulop@users.noreply.github.com>
2022-01-30 03:22:48 +0800

    2nd commit

---
+++ src/component/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Main () {
+    return (
+        <div className="main_div">
+            <h1 className='main'>Fun facts about React</h1>
+            <ul>
+                <li>Released in 2013</li>
+                <li>Created by Jordan Walke</li>
+                <li>Maintained by Facebook</li>
+                <li>Has well over 100k stars in Github</li>
+            </ul>
+            
+        </div>
+    )    
+}
---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+export default function Navbar () {
+    return (       
+            <nav>
+                <img className="logo" src="./logo512.png" />
+                <h1>ReactFacts</h1>
+                <h2>React course project-1</h2>
+            </nav> 
+    )
+}
